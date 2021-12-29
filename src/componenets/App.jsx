3fa50a003a6c8992a406ede6dfb4dfb47bda0c79
Albert Takács <albitakacs@gmail.com>
2021-12-29 17:40:15 +0100

    add react

--- src/componenets/App.jsx
+++ src/componenets/App.jsx
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
+import data from './Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
+import friends from './FriendList/friends.json';
+import { FriendList } from './FriendList/FriendList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload Stats" stats={data} />
+      <FriendList friends={friends} />
+    </>
   );
 }
 
---
+++ src/componenets/FriendList/FriendList.jsx
@@ -0,0 +1,5 @@
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return <ul class="friend-list"></ul>;
+};
